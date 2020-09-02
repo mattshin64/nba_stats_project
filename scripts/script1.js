fa96903c80c69642d554527d14d6939de3cebc42
mattshin64 <mattshin64@gmail.com>
2020-09-02 14:59:44 -0700

    script is able to traverse all game events in a certain year

--- scripts/script1.js
+++ scripts/script1.js
@@ -4,9 +4,20 @@ axios.get('https://data.nba.net/10s/prod/v1/2019/schedule.json')
     .then((data)=>  {
         let arrofgames = data.data.league.standard
         for(var i = 0; i < arrofgames.length; i += 1){
-            console.log(arrofgames[i].startDateEastern)
             gameIds[arrofgames[i].startDateEastern] = arrofgames[i].gameId
         }
-        console.log(gameIds)
+    })
+    .then(() => {
+        for(var key in gameIds){
+            let gameId = gameIds[key]
+            axios.get(`http://data.nba.net/data/10s/json/cms/noseason/game/${key}/${gameId}/pbp_all.json`)
+            .then((data)=> {
+                let arrofplays = data.data.sports_content.game.play
+                for(var k = 0; k < arrofplays.length; k += 1){
+                    console.log(arrofplays[k].description)
+                }
+            })
+            .catch((err) => console.error(err))
+        }
     })
     .catch((err) => console.error(err))
